299a2b7c0e8c36925e053fa8cc02c3b436f95e89
wgr1984 <w.reithmeier@gmail.com>
2020-03-31 13:25:45 +0200

    added router

--- src/lib.rs
+++ src/lib.rs
@@ -1,83 +1,89 @@
 #![recursion_limit = "256"]
 
-use wasm_bindgen::prelude::*;
-use yew::services::ConsoleService;
-use yew::{html, Component, ComponentLink, Html, ShouldRender};
-
 mod material {
     pub mod topbar;
 }
 
+mod components {
+    pub mod main_page;
+}
+
+use wasm_bindgen::prelude::*;
+use yew::services::ConsoleService;
+use yew::{html, Component, ComponentLink, Html, ShouldRender};
+use yew_router::{prelude::*, Switch};
+use yew_router::switch::{AllowMissing, Permissive};
+
 use material::topbar::*;
+use components::main_page::*;
 
 // Use `wee_alloc` as the global allocator.
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-pub struct Model {
-    link: ComponentLink<Self>,
-    console: ConsoleService,
-    value: i64,
-}
-
-pub enum Msg {
-    Increment,
-    Decrement,
-    Bulk(Vec<Msg>),
-}
+pub struct Model {}
 
 impl Component for Model {
-    type Message = Msg;
+    type Message = ();
     type Properties = ();
 
-    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
-        Model {
-            link,
-            console: ConsoleService::new(),
-            value: 0,
-        }
+    fn create(_: Self::Properties, _link: ComponentLink<Self>) -> Self {
+        Model {}
     }
 
-    fn update(&mut self, msg: Self::Message) -> ShouldRender {
-        match msg {
-            Msg::Increment => {
-                self.value = self.value + 1;
-                self.console.log("plus one");
-            }
-            Msg::Decrement => {
-                self.value = self.value - 1;
-                self.console.log("minus one");
-            }
-            Msg::Bulk(list) => {
-                for msg in list {
-                    self.update(msg);
-                    self.console.log("Bulk action");
-                }
-            }
-        }
-        true
+    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+        false
     }
 
     fn view(&self) -> Html {
         html! {
-            // <div>
-            //     <nav class="menu">
-            //         <button onclick=self.link.callback(|_| Msg::Increment)>
-            //             { "Increment" }
-            //         </button>
-            //         <button onclick=self.link.callback(|_| Msg::Decrement)>
-            //             { "Decrement" }
-            //         </button>
-            //         <button onclick=self.link.batch_callback(|_| vec![Msg::Increment, Msg::Increment])>
-            //             { "Increment Twice" }
-            //         </button>
-            //     </nav>
-            //     <p>{ self.value }</p>
-            // </div>
-            <Topbar title="What 2 by where?"/>
+            <div>
+                <Topbar title="What 2 by where?"/>
+                <div class="mdc-top-app-bar--fixed-adjust">
+                    <Router<AppRoute>
+                        render = Router::render(|switch: AppRoute| {
+                            match switch {
+                                AppRoute::Index => html! { <MaingPageModel /> },
+                                // AppRoute::A(AllowMissing(route)) => html!{<AModel route = route />},
+                                // AppRoute::B(route) => {
+                                //     let route: b_component::Props = route.into();
+                                //     html!{<BModel with route/>}
+                                // },
+                                // AppRoute::C => html!{<CModel />},
+                                AppRoute::E(string) => html!{format!("hello {}", string)},
+                                AppRoute::PageNotFound(Permissive(None)) => html!{"Page not found"},
+                                AppRoute::PageNotFound(Permissive(Some(missed_route))) => html!{format!("Page '{}' not found", missed_route)}
+                            }
+                        })
+                        redirect = Router::redirect(|route: Route| {
+                            AppRoute::PageNotFound(Permissive(Some(route.route)))
+                        })
+                    />
+                </div>
+            </div>
         }
     }
 }
+
+#[derive(Debug, Switch, Clone)]
+pub enum AppRoute {
+    #[to = "/"]
+    Index,
+    // #[to = "/a{*:inner}"]
+    // A(AllowMissing<ARoute>),
+    // #[to = "/b{*:inner}"]
+    // B(BRoute),
+    // #[to = "/c"]
+    // C,
+    #[to = "/e/{string}"]
+    E(String),
+    #[to = "/page-not-found"]
+    PageNotFound(Permissive<String>),
+}
+
+#[derive(Debug, Switch, PartialEq, Clone, Copy)]
+#[to = "/c"]
+pub struct ARoute;
 
 #[wasm_bindgen(start)]
 pub fn render() {
